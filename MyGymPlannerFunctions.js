2c971a932c73b9122d1d2a18a5b51ab0efde05c6
Ryan <ryansharpe@gmail.com>
2019-10-30 09:39:17 +0000

    Project Upload

--- MyGymPlannerFunctions.js
+++ MyGymPlannerFunctions.js
@@ -1,3 +1,7 @@
+		var idUP;
+		
+		//Visibility of Add Form
+
 function AddExercise(){
 var Ref1=document.getElementById("Ex");
 var Ref2=document.getElementById("MID");
@@ -7,40 +11,32 @@ var refadd= document.getElementById("AddForm");
 refadd.style.visibility="visible";
 var refedit= document.getElementById("EditForm");
 refedit.style.visibility="hidden";
-var refdelete= document.getElementById("DeleteForm");
-refdelete.style.visibility="hidden";
-refadd.style="width:275px;height:315px;position:fixed;border-radius:25px;background-color:grey;border:2px solid #A22F16;top:50%;left:50%;margin-top: -100px;margin-left: -100px";
+refadd.style="width:325px;height:315px;position:fixed;border-radius:25px;background-color:grey;border:2px solid #A22F16;top:50%;left:50%;margin-top: -100px;margin-left: -100px";
 }
+
+		//Visibility of Edit Form
+
+		
 
 function EditExercise(){
 var refadd= document.getElementById("AddForm");
 refadd.style.visibility="hidden";
 var refedit= document.getElementById("EditForm");
 refedit.style.visibility="visible";
-var refdelete= document.getElementById("DeleteForm");
-refdelete.style.visibility="hidden";
-refedit.style="width:275px;height:225px;position:fixed;border-radius:25px;background-color:grey;border:2px solid #A22F16;top:50%;left:50%;margin-top: -100px;margin-left: -100px";
+refedit.style="width:375px;height:225px;position:fixed;border-radius:25px;background-color:grey;border:2px solid #A22F16;top:50%;left:50%;margin-top: -100px;margin-left: -100px";
 }
 
-function DeleteExercise(){
-var refadd= document.getElementById("AddForm");
-refadd.style.visibility="hidden";
-var refedit= document.getElementById("EditForm");
-refedit.style.visibility="hidden";
-var refdelete= document.getElementById("DeleteForm");
-refdelete.style.visibility="visible";
-refdelete.style="width:275px;height:175px;position:fixed;border-radius:25px;background-color:grey;border:2px solid #A22F16;top:50%;left:50%;margin-top: -100px;margin-left: -100px";
-}
+		//Menu Close Button Function
 
 function CloseMenu(){
 	var refadd= document.getElementById("AddForm");
 	refadd.style.visibility="hidden";
 	var refedit= document.getElementById("EditForm");
 	refedit.style.visibility="hidden";
-	var refdelete= document.getElementById("DeleteForm");
-	refdelete.style.visibility="hidden";
 	window.alert("Form Closed");
 }
+
+		// Error Messages for Empty Fields (Add Form)
 
 function checkValues1(){
 	var Ref1=document.getElementById("Ex");
@@ -50,58 +46,182 @@ function checkValues1(){
 		return false;
 	}
 	if(Ref2.value==""){
-		alert("Muscle ID is Missing");
+		alert("Muscle Group is Missing");
 		return false;
 	}
 	
 	var refadd= document.getElementById("AddForm");
 	refadd.style.visibility="hidden";
 	window.alert("Exercise Added");
-	location.reload(true);
+	return true;
 }
 
+		// Error Messages for Empty Fields (Edit Form)
+
 function checkValues2(){
-	var Ref1=document.getElementById("Ex1");
-	var Ref2=document.getElementById("MID1");
+	var Ref1=document.getElementById("Update");
+	var Ref2=document.getElementById("Ed");
 	if(Ref1.value==""){
-		alert("Exercise is Missing");
+		alert("What You Want To Change Is Missing");
 		return false;
 	}
 	if(Ref2.value==""){
-		alert("Muscle ID is Missing");
+		alert("You Haven't Entered A Change");
 		return false;
 	}
 	var refedit= document.getElementById("EditForm");
 	refedit.style.visibility="hidden";
 	window.alert("Exercise Altered");
-	location.reload(true);
+	return true;
 }
 
-function checkValues3(){
-	var Ref1=document.getElementById("ExID");
-	if(Ref1.value==""){
-		alert("Enter the Exercise ID you wish to Delete");
-		return false;
-	}
-	else {
-	var refdelete= document.getElementById("DeleteForm");
-	refdelete.style.visibility="hidden";
-	location.reload(true);
-	window.alert("Deletion Confirmed");
-	}
-}
+		// Clear Text on Form when Clicked
 
 function clearText(T){
 	T.value="";
 }
 
+		//Reload Function
+
 function reload(){
 	location.reload(true);
 }
 
-function postData(form){
-	for (let inputty of form){
-	console.log(inputty.value);
+		// Show All Records Function
+
+function getexistingrecs(){
+	const Http = new XMLHttpRequest();
+	const url="http://localhost:6464/exercise";
+	Http.open("GET", url);
+	Http.onreadystatechange = function(e){
+	   if (Http.readyState==4){
+		   var maintable = document.getElementById("AllExBody");
+		    maintable.innerHTML="";
+	   data=JSON.parse(Http.responseText);
+	   data.forEach(function(item){
+		  var weekday=document.getElementById(item.weekday+"Body");
+		   var musclegroup=document.createElement("td");
+		   var exercise=document.createElement("td");
+		   var exerciseid=document.createElement("td");
+		   var buttonDel = document.createElement("td");
+		   var buttonUpd = document.createElement("td");
+		   musclegroup.innerHTML=item.musclegroup;
+		   exercise.innerHTML=item.exercise;
+		   exerciseid.innerHTML=item.exerciseid;
+
+		   let button = document.createElement("button");
+		   button.innerHTML= "X";
+		   button.type="button";
+		   button.className = "btn tableDel";
+		   button.addEventListener("click", function() {
+			   deleteData(item.exerciseid);
+		   });
+		   buttonDel.appendChild(button);
+
+		   let buttonU = document.createElement("button");
+		   buttonU.innerHTML= "Edit";
+		   buttonU.type="button";
+		   buttonU.className = "btn tableUpd";
+		   buttonU.addEventListener("click", function() {
+			   EditExercise();
+			   idUP = item.exerciseid;
+		   });
+		   buttonUpd.appendChild(buttonU);
+		  
+		   let mainRow=document.createElement("tr");
+		   
+		   mainRow.appendChild(musclegroup);
+			mainRow.appendChild(exercise);
+		   mainRow.appendChild(exerciseid);
+			mainRow.appendChild(buttonDel);
+			mainRow.appendChild(buttonUpd);
+		   maintable.appendChild(mainRow);
+		   
+		   let x=mainRow.cloneNode(true);
+		   weekday.appendChild(x);
+
+
+	   });
 	}
-	return false;
+	}
+	Http.send();
+	}
+	window.onload=getexistingrecs();
+
+			// Delete Function
+
+	function deleteData(id){
+		var Http=new XMLHttpRequest();
+		Http.open("DELETE", "http://localhost:6464/exercise/" + id);
+		Http.setRequestHeader("Content-Type", "application/json");
+
+		Http.onload=function(){
+			getexistingrecs();
+		}
+
+		Http.send();
+		return false;
+	}
+
+function postData(form){
+    if (checkValues1()){
+        var body= {};
+        for(var inputty of form){
+            if(inputty.name){
+
+				if(inputty.type=="radio"){
+					if(inputty.checked){
+						body [inputty.name]= inputty.value;
+					}
+				}else{
+					body [inputty.name]= inputty.value;
+				}
+            }
+        
+		}
+
+		var data = JSON.stringify(body);
+		var Http= new XMLHttpRequest();
+        Http.open("POST", 'http://localhost:6464/exercise');
+        Http.setRequestHeader("Content-Type", "application/json");
+        Http.onload= function(){
+            getexistingrecs();
+        }
+            
+        
+		Http.send(data);
+	}
+        return false;
+	}
+
+function editData(){
+
+    var url ;
+	var upD;
+	
+	if(checkValues2()){
+	if(document.getElementById("Update").value=="Exercise"){
+        upD=document.getElementById("Ed").value;
+        url="http://localhost:6464/exerciseedit1/"+idUP+"/"+upD;
+        
+    }
+    if(document.getElementById("Update").value=="Muscle Group"){
+        upD=document.getElementById("Ed").value;
+        url="http://localhost:6464/exerciseedit2/"+idUP+"/"+upD;
+        
+    }
+	
+    var Http = new XMLHttpRequest();
+        Http.open("PUT", url);
+        Http.setRequestHeader("Content-Type", "application/json");
+        Http.onload= function(){
+            getexistingrecs();
+		}
+	
+		Http.send();
+	}
+        return false;
+	
 }
+	
+	
